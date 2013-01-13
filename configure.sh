45f5c1984a9de0137fe11dcbd587bf974687fd7f
Marco Maggi <marco.maggi-ipsu@poste.it>
2013-01-13 20:36:36 +0100

    reorganised tests to have both C and f77 check programs

--- configure.sh
+++ configure.sh
@@ -10,10 +10,11 @@ prefix=/usr/local
     --config-cache                              \
     --cache-file=../config.cache                \
     --enable-maintainer-mode                    \
-    --disable-static --enable-shared            \
     --prefix="${prefix}"                        \
     CFLAGS='-O3 -march=i686 -mtune=i686'        \
     FFLAGS='-O3 -march=i686 -mtune=i686'        \
     "$@"
+
+#    --disable-static --enable-shared
 
 ### end of file
---
+++ tests-f77/cal2jd.f
@@ -0,0 +1,65 @@
+* Part of: ATSOFA
+* Contents: test for the function "cal2jd"
+* Date: Sun Jan 13, 2013
+*
+* Abstract
+*
+*       Test conversion  Gregorian Calendar to Julian  Date.  The Julian
+*       Date is  the interval  of time  in days and  fractions of  a day
+*       since -4714-11-24T12:00:00Z (November 24, -4714 at noon, UTC).
+*
+* Copyright (C) 2013 Marco Maggi <marco.maggi-ipsu@poste.it>
+*
+* See the COPYING file for copying permissions.
+
+      program cal2jd_f
+      implicit none
+
+      integer exit_code
+      double precision result
+*     Julian day number at 12:00.
+      double precision expected_result
+
+*     Input
+*     Year in Gregorian calendar.
+      integer iy
+*     Month in Gregorian calendar.
+      integer im
+*     Day in Gregorian calendar.
+      integer id
+
+*     Output
+*     Modified Julian Day zero-point: always 2400000.5.
+      double precision djm0
+*     Modified Julian Date for 0 hrs.
+      double precision djm
+*     Return code
+*     0  = OK
+*     -1 = bad year   (Note 3: JD not computed)
+*     -2 = bad month  (JD not computed)
+*     -3 = bad day    (JD computed)
+      integer status
+
+      exit_code = 1
+      result = 0.0
+      expected_result = 2456306.0
+
+      iy = 2013
+      im = 1
+      id = 13
+
+      call cal2jd(iy, im, id, djm0, djm, status)
+      if (0.EQ.status) then
+         result = djm + djm0 + 0.5
+      end if
+
+      if ((0.EQ.status) .and. (expected_result.EQ.result)) then
+         exit_code = 0
+      else
+         exit_code = 1
+      end if
+c      stop
+      call exit (exit_code)
+      end
+
+*** end of file
