9c2147855cfc90a42c44b9e5d341ad7c3607a7a9
Marco Maggi <marco.maggi-ipsu@poste.it>
2014-01-28 09:21:53 +0100

    fixed admin script to allow installation on 64-bit platforms

--- configure.sh
+++ configure.sh
@@ -5,12 +5,17 @@
 set -xe
 
 prefix=/usr/local
+if test -d /lib64
+then libdir=${prefix}/lib64
+else libdir=${prefix}/lib
+fi
 
 ../configure \
     --config-cache                              \
     --cache-file=../config.cache                \
-    --prefix="${prefix}"                        \
     --disable-static --enable-shared            \
+    --prefix="${prefix}"                        \
+    --libdir="${libdir}"                        \
     CFLAGS='-O3'				\
     FFLAGS='-O3'				\
     "$@"
